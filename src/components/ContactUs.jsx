e5bc4b215e9bb182a03a313f3d9796a7115aafd5
Vadgyrif <vadikwizz@gmail.com>
2025-04-05 01:32:33 +0300

    Fix: added unique title to iframe elements in ContactUs component

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -23,6 +23,7 @@ const ContactUs = () => {
                 <h3>Наше місцезнаходження на карті</h3>
                 <div className="google-map">
                     <iframe
+                        title="unique-description-for-iframe" 
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2524.516749755744!2d25.323920376968942!3d50.74745126593427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472599ef3fa39e17%3A0xd5b8634e98077a56!2z0KbQo9CcLCDQv9GA0L7RgdC_0LXQutGCINCS0L7Qu9GWLCAxLCDQm9GD0YbRjNC6LCDQktC-0LvQuNC90YHRjNC60LAg0L7QsdC70LDRgdGC0YwsIDQzMDAw!5e0!3m2!1suk!2sua!4v1743768312826!5m2!1suk!2sua" 
                         style={{border: 0}}
                         allowFullScreen=""
