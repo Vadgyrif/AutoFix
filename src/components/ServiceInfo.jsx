e81b353c32aebccba6a8a19aa8181b40aa800b6d
Vadgyrif <vadikwizz@gmail.com>
2025-04-05 23:17:38 +0300

    scroll bug

--- src/components/ServiceInfo.jsx
+++ src/components/ServiceInfo.jsx
@@ -6,40 +6,38 @@ import Money from "../assets/money.png"
 import Brush from "../assets/brush.png"
 import BeforeAfterSliderComponent from "./BeforeAfterScroll";
 
-const ServiceInfo = () => {
-    return (
+const  ServiceInfo = () => {
+    return(
         <Element name="about">
-            <div className="ServiceInfoWrapper">
-                <div className="ServiceInfo">
-                    <div className="ServiceInfo_image"> 
-                        <BeforeAfterSliderComponent />
-                    </div>
-                    <div className="ServiceInfo-About">
-                        <div className="ServiceInfo_title">
-                            Про послугу
-                        </div>
-                        <div className="ServiceInfo_text">
-                            Метод Paintless Dent Repair (PDR) – це сучасна технологія
-                            усунення вм’ятин без необхідності шпаклювання чи фарбування. Використовуючи 
-                            спеціальні інструменти, наш майстер дбайливо вирівнює поверхню, повертаючи 
-                            вашому авто первозданний вигляд.
-                        </div>
-                        <ul className="ServiceInfo_list"> 
-                            <li>
-                                <img className="ServiceInfo_icon" src={Brush} alt="" /> Збереження заводського покриття - жодного шпаклювання чи перефарбування 
-                            </li>
-                            <li>
-                                <img className="ServiceInfo_icon" src={Money} alt="" /> Вигідна ціна - дешевше за традиційний кузовний ремонт 
-                            </li>
-                            <li>
-                                <img className="ServiceInfo_icon" src={Ecology} alt="" /> Екологічність - відсутність шкідливих матеріалів та випарів 
-                            </li>
-                        </ul>
-                    </div>
+            <div data-aos="fade-up" className="ServiceInfo">
+                <div className="ServiceInfo_image"> 
+                    <BeforeAfterSliderComponent />
                 </div>
+                <div className="ServiceInfo-About">
+                    <div className="ServiceInfo_title">
+                        Про послугу
+                    </div>
+                    <div className="ServiceInfo_text"> Метод Paintless Dent Repair (PDR) – це сучасна технологія
+                        усунення вм’ятин без необхідності шпаклювання чи фарбування. Використовуючи 
+                        спеціальні інструменти, наш майстер дбайливо вирівнює поверхню, повертаючи 
+                        вашому авто первозданний вигляд.</div>
+                    <ul className="ServiceInfo_list" > 
+                        <li>
+                            <img className="ServiceInfo_icon" src={Brush} alt="" /> Збереження заводського покриття - жодного шпаклювання чи перефарбування 
+                        </li>
+                        <li>
+                            <img className="ServiceInfo_icon" src={Money} alt="" />Вигідна ціна - дешевше за традиційний кузовний ремонт </li>
+                        <li>
+                            <img className="ServiceInfo_icon" src={Ecology} alt="5" />    Екологічність - відсутність шкідливих матеріалів та випарів 
+                        </li>
+                    </ul>
+                </div>
+                
             </div>
         </Element>
-    );
-};
+    )
 
-export default ServiceInfo;
+
+}
+
+export default ServiceInfo; 
